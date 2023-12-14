63c8b32347e2a3f38f2f3381815a47e81954be5c
Daniel Dahlgren <daniel.dahlgren@gavle.se>
2023-12-14 14:08:00 +0100

    Added

--- calc.js
+++ calc.js
@@ -1,7 +1,13 @@
-function calculateScores() {
+function calculateScores(totalPlayerCount) {
     const input = document.getElementById("positionsInput").value;
+    
+    if(isNaN(totalPlayerCount))
+    {
+        totalPlayerCount = document.getElementById("totalInput").value;
+    }
+    
     const ourPositions = getPositions(input);
-    const opponentPositions = getMissingNumbers(ourPositions);
+    const opponentPositions = getMissingNumbers(ourPositions, checkAndSetBoundaries(totalPlayerCount, 1, 100));
     const ourScore = calculateTeamScore(ourPositions);
     const opponentScore = calculateTeamScore(opponentPositions);
     const totalScore = ourScore + opponentScore;
@@ -14,10 +20,11 @@ function calculateScores() {
     opponentScoreElement.textContent = opponentScore  + ' (players: ' + opponentPositions.length + ')';
 
     // Only show results if the sum is correct
-    if (totalScore == 4364) {
-        resultElement.classList.remove("d-none");
+    if (totalScore == getExpectedTotalScore(totalPlayerCount)) {
+         resultElement.classList.remove("d-none");
     }
-    else if (!resultElement.classList.contains('d-none')) {
+    else if (!resultElement.classList.contains('d-none')) 
+    {
         resultElement.classList.add('d-none');
     }
 }
@@ -26,6 +33,16 @@ function handleKeyPress(event) {
     if (event.key === "Enter") {
         event.preventDefault();
         calculateScores(); 
+    }
+}
+
+function checkAndSetBoundaries(number, lowerBound, upperBound) {
+    if (number < lowerBound) {
+        return lowerBound;
+    } else if (number > upperBound) {
+        return upperBound;
+    } else {
+        return number;
     }
 }
 
@@ -45,15 +62,30 @@ function getPositions(input) {
     return positions;
 }
 
-function calculateTeamScore(positions) {
-    const scoresList = [
-        300, 280, 262, 244, 228, 213, 198, 185, 173, 161, 
-        150, 140, 131, 122, 114, 107, 99, 93, 87, 81, 
-        75, 70, 66, 61, 57, 54, 50, 47, 44, 41, 
-        38, 35, 33, 31, 29, 27, 25, 24, 22, 21, 
-        19, 18, 17, 16, 15, 14, 13, 12, 11, 11
-    ];
+const scoresList = [
+    300, 280, 262, 244, 228, 213, 198, 185, 173, 161, 
+    150, 140, 131, 122, 114, 107, 99, 93, 87, 81, 
+    75, 70, 66, 61, 57, 54, 50, 47, 44, 41, 
+    38, 35, 33, 31, 29, 27, 25, 24, 22, 21, 
+    19, 18, 17, 16, 15, 14, 13, 12, 11, 11,
+    10, 9, 9, 8, 8, 7, 7, 6, 6, 6,
+    5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 
+    3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 
+    2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+];
 
+function getExpectedTotalScore(playerCount)
+{
+    let totalScore = 0;
+    for (let i = 1; i <= playerCount; i++) {
+        totalScore += scoresList[i - 1];
+    }
+    console.log(totalScore);
+    return totalScore;
+}
+
+function calculateTeamScore(positions) {
     let totalScore = 0;
     for (const position of positions) {
         totalScore += scoresList[position - 1];
@@ -61,8 +93,8 @@ function calculateTeamScore(positions) {
     return totalScore;
 }
 
-function getMissingNumbers(positions) {
-    const allPositions = Array.from({ length: 50 }, (_, i) => i + 1);
+function getMissingNumbers(positions, totalPlayerCount) {
+    const allPositions = Array.from({ length: totalPlayerCount }, (_, i) => i + 1);
     const missingNumbers = allPositions.filter((pos) => !positions.includes(pos));
     return missingNumbers;
 }
